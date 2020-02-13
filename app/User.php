6900aaf19f509694f7e081d61bb9e4b4276176a2
Mirta <mirta.lopez.cardona@gmail.com>
2020-02-13 18:55:10 +0100

    feat(#2): Add JWTSubject implementation to User model

--- app/User.php
+++ app/User.php
@@ -5,10 +5,17 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+//JWT Add use
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+//JWT Add implementation of Interface
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+    /**
+     * Database table. If commented, Pluralize will set it to "Users"
+     */
+    //protected $table = "usuarios";
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +43,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //JWT
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    //JWT
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
